9e73f300950cf97f120178af1a836854f23742ba
Mike Zbrozek <mzbrozek@cnm.edu>
2023-09-05 17:15:07 -0600

    Lecture exercises Week 2

---
+++ CourseAssignments/Week2/BusTicket.java
@@ -0,0 +1,10 @@
+// Name: Michael Zbrozek
+// Class: 1152:101
+// Assignment: Week2: Bus Ticket
+// Purpose: Determine the type of ticket the user is purchasing.
+// FileName: "BusTicket.java"
+
+// ** Criteria ** //
+// Senior Ticket age greater than or equal to 65
+// Adult Ticket age greater than or equal to 18
+// Child’s Ticket age less than 18
